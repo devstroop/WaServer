9e906341670b7b7bd832642d2a49a95bb1600e87
Akash Shah <akash@devstroop.com>
2026-08-28 12:06:10 +0530

    feat(web): add application shells
    Refs #78

---
+++ web/src/components/shells/index.ts
@@ -0,0 +1,9 @@
+export { PublicShell } from "./PublicShell";
+export type { PublicShellProps } from "./PublicShell";
+export { AuthShell } from "./AuthShell";
+export type { AuthShellProps } from "./AuthShell";
+export { UserShell } from "./UserShell";
+export type { UserShellProps } from "./UserShell";
+export { AdminShell } from "./AdminShell";
+export type { AdminShellProps } from "./AdminShell";
+export { useResponsiveSidebar } from "./useResponsiveSidebar";
